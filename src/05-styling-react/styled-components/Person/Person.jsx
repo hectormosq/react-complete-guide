bee0ee55661d7520d622b4d4c861b38413f1f945
Hector <hjmosquera@eservicios.indra.es>
2020-03-13 11:14:50 +0100

    Se implementa storybook para visualziacion y se prueban formas de aplicar estilos a la aplicacion

--- src/05-styling-react/styled-components/Person/Person.jsx
+++ src/05-styling-react/styled-components/Person/Person.jsx
@@ -1,17 +1,28 @@
 import React from "react";
+import styled from "styled-components";
 import "./Person.css";
 
 const person = props => {
   const { name, age, children, click, changed } = props;
+  const StyledDiv = styled.div`
+    width: 60%;
+    margin: 10px auto;
+    border: 1px solid #eee;
+    padding: 16px;
+    text-align: center;
+    @media (min-width: 500px) {
+      width: 450px;
+    }
+  `;
 
   return (
-    <div className="Person">
+    <StyledDiv>
       <p onClick={click}>
         Im a {name}! and I am {age} years old
       </p>
       <p>{children}</p>
       <input type="text" onChange={changed} value={name} />
-    </div>
+    </StyledDiv>
   );
 };
 export default person;
