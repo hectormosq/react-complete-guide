b4edc120d3c0d13403fc3748c61a70b0e9ae83fd
Hector <hjmosquera@eservicios.indra.es>
2020-03-17 18:15:22 +0100

    Updates

---
+++ precache-manifest.abe413e6dbcd81b60a2ff6d3de0997f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c73b118cb28ad789058d4d0e0be4a7ce",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.a4729fdb3911181a083a.bundle.js"
+  },
+  {
+    "url": "runtime~main.a4729fdb3911181a083a.bundle.js"
+  },
+  {
+    "revision": "0ae04fd91eb91a1f56d8",
+    "url": "static/css/main.6fd9c79e.chunk.css"
+  },
+  {
+    "url": "vendors~main.a4729fdb3911181a083a.bundle.js"
+  },
+  {
+    "url": "vendors~main.a4729fdb3911181a083a.bundle.js.LICENSE.txt"
+  }
+]);
